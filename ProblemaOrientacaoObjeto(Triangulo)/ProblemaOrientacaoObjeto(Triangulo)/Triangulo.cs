6597a9de873baf3738ad02e95c5a742f5759a26e
Willian Couto <willian_994@hotmail.com>
2022-08-18 17:00:45 -0300

    Criacao de metodos

--- ProblemaOrientacaoObjeto(Triangulo)/ProblemaOrientacaoObjeto(Triangulo)/Triangulo.cs
+++ ProblemaOrientacaoObjeto(Triangulo)/ProblemaOrientacaoObjeto(Triangulo)/Triangulo.cs
@@ -1,9 +1,19 @@
-﻿namespace ProblemaOrientacaoObjeto_Triangulo_
+﻿using System;
+
+namespace ProblemaOrientacaoObjeto_Triangulo_
 {
     class Triangulo
     {
         public double A;
         public double B;
         public double C;
+
+        public double Area()
+        {
+            double p = (A + B + C) / 2.0;
+            double area = Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+
+            return area;
+        }
     }
 }
